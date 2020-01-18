d4fd5a6cfe332dd31d8a61337792b72c59cdc681
aindrajaya <arista.indrajaya@gmail.com>
2020-01-18 22:39:02 +0700

    Coding running

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const Robots = [
+    {
+        id:1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+      },
+      {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+      },
+      {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+      },
+      {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca'
+      },
+      {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info'
+      },
+    {
+        id:7,
+        name: 'Alberto Guidante',
+        username: 'Ablert',
+        email: 'Alberto@fake.ru'
+    },
+    {
+        id:8,
+        name: 'Nicholas Ronasnans',
+        username: 'Maxime_Niewnow',
+        email: 'Sherwood@rosamond.me'
+    },
+    {
+        id:9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim.McDermott@dana.io'
+    },
+    {
+        id:10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padber@karina.biz'
+    }
+];
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import CardList from '/CardList';
+import { Robots } from './Robots';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Card id={Robots[0].id} name={Robots[0].name} email={Robots[0].email}/>
+        <Card id={Robots[1].id} name={Robots[1].name} email={Robots[1].email}/>
+        <Card id={Robots[2].id} name={Robots[2].name} email={Robots[2].email}/>
+    </div>
+, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();

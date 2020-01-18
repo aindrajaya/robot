ca27d444c72e69a1aa698f55814edbaaff2b47e9
aindrajaya <arista.indrajaya@gmail.com>
2020-01-18 23:14:21 +0700

    Added Searchbox

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CardList from './CardList';
+import {Robots} from './Robots';
+import SearchBox from './SearchBox';
+
+const App = () => {
+    return(
+        <div className='tc'>
+            <h1>MrRobot</h1>
+            <SearchBox />
+            <CardList Robots={Robots}/>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
-import CardList from '/CardList';
-import { Robots } from './Robots';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 ReactDOM.render(
     <div>
-        <Card id={Robots[0].id} name={Robots[0].name} email={Robots[0].email}/>
-        <Card id={Robots[1].id} name={Robots[1].name} email={Robots[1].email}/>
-        <Card id={Robots[2].id} name={Robots[2].name} email={Robots[2].email}/>
+        <App />
     </div>
 , document.getElementById('root'));
 

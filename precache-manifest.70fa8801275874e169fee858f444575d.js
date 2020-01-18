69cc2ac98984322b88efcfd0ea52560d7502dc96
aindrajaya <arista.indrajaya@gmail.com>
2020-01-19 02:44:11 +0700

    Updates

---
+++ precache-manifest.70fa8801275874e169fee858f444575d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020ed9d093b23bd7a4b381bbabb6a1a9",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "9cfd3cfe845825fa01af",
+    "url": "/robot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fdf73e288a9536ff30e5",
+    "url": "/robot/static/css/main.ce0c37cb.chunk.css"
+  },
+  {
+    "revision": "9cfd3cfe845825fa01af",
+    "url": "/robot/static/js/2.c82709b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robot/static/js/2.c82709b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdf73e288a9536ff30e5",
+    "url": "/robot/static/js/main.b441af39.chunk.js"
+  },
+  {
+    "revision": "bd965e9f43792e3ff1fb",
+    "url": "/robot/static/js/runtime-main.8b3ae5b2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);

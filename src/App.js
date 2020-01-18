e4de27155ac2d7d73e568409af65365453b23987
aindrajaya <arista.indrajaya@gmail.com>
2020-01-19 01:34:18 +0700

    set state for search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import {Robots} from './Robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return(
-        <div className='tc'>
-            <h1>MrRobot</h1>
-            <SearchBox />
-            <CardList Robots={Robots}/>
-        </div>
-    );
+class App extends Component {
+    //it declares two states: robot and searchfield
+    //all of the component in this app syntax that has state can use constructor function to use this.state
+    constructor(){
+        super()
+        this.state = { //this states is what changes in an app
+            Robots: Robots,
+            searchfield: '' //searchfield is always empty
+        }
+    }
+    
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value}) 
+    }
+
+    render(){
+        const filteredRobots = this.state.Robots.filter(Robots => {
+            return Robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return(
+            <div className='tc'>
+                <h1>MrRobot</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList Robots={filteredRobots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
